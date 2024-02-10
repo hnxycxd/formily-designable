28e4dcbf8a95d5d7e8a3492c0e29e02b31ff797c
程旭东 <xiyang080@126.com>
2024-02-10 16:51:32 +0800

    playground

---
+++ craco.config.ts
@@ -0,0 +1,30 @@
+import CracoLessPlugin from 'craco-less'
+import { CracoConfig } from '@craco/types'
+// import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
+
+const cracoConfig: CracoConfig = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    // plugins: {
+    //   add: [new MonacoWebpackPlugin()],
+    // },
+    // configure: (webpackConfig) => {
+    //   webpackConfig.plugins.push(new MonacoWebpackPlugin())
+    //   return webpackConfig
+    // },
+  },
+}
+
+export default cracoConfig
